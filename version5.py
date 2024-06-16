f389cb3c4de40ebe739a0571d6675cb49a3f9a65
Bisrat Getachew <bisratgetachew0449@gmail.com>
2024-06-16 10:48:57 +0300

    another one

--- version5.py
+++ version5.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
                     print(reply_text)
                     getting_responses(gs, pre_message, reply_text)
                     send_message_bis(585511605, reply_text)
-                    time.sleep(3)
+                    time.sleep(4)
 
 
 
