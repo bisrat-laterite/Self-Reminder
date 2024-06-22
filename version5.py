673933b89875b02472c2c457542379e1cc46e446
Bisrat Getachew <bisratgetachew0449@gmail.com>
2024-06-22 13:52:19 +0300

    Modified str_to_dict function

--- version5.py
+++ version5.py
@@ -47,7 +47,7 @@ def str_to_dict(string):
     # split the string into key-value pairs
     pairs = string.split('\n')
     # print(pairs)
-    pre= {key[0].rstrip().lstrip():key[1].rstrip().lstrip() for key in (pair.split(':') for pair in pairs) if key[0].rstrip().lstrip() in ['HHID', 'Variable']}
+    pre= {key[0].rstrip().lstrip():key[1].rstrip().lstrip() for key in (pair.split(':') for pair in pairs) if key[0].rstrip().lstrip() in ['HHID', 'Variable', 'FC Name']}
     # print(pre)
     return pre
   
