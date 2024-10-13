2407dcf6bea4bb37274a527ecbe56235fb9a2a10
Bisrat Getachew <bisratgetachew0449@gmail.com>
2024-10-14 00:02:27 +0300

    send dq

--- version5.py
+++ version5.py
@@ -3,8 +3,26 @@ import pandas as pd
 import requests
 import datetime
 import time
+import os
 
 
+TELEGRAM_BOT_TOKEN = os.getenv("bot_token")
+TELEGRAM_API_URL = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/'
+main_sheet_key='1kq0JxL3PxB4yxZfBv_2_WOEHvy6kptP7jqB31v0XZoU'
+main_sheet_name='project_database'
+
+# google_credentials = os.getenv("GOOGLE_CREDENTIALS_BASE64")
+
+
+def read_gsheet(key, sheet):
+    gc=gspread.service_account(filename='credentials.json')
+    key_=key ### change to the sheets for regron
+    ### Reading in the specific googles sheets file
+    sh=gc.open_by_key(key_)
+    sheet=sheet
+    gs=sh.worksheet(sheet)
+    #update_id = gs.cell(1, 2).value
+    return gs
 
 ##function to send the message of the data quality questions 
 def send_message_bis(chat_id,text):
@@ -135,76 +153,108 @@ def getting_responses(main_text, text):
 
 # while True:
     # reading messages
+if __name__=="__main__":
+    #### sending data quality
+    main=pd.DataFrame(read_gsheet(main_sheet_key, main_sheet_name).get_all_records())
+    main=main[main['status']=="Ongoing"]
+    dict_pre=main[['key', 'project_id']]
+    dict_from_columns = dict_pre.set_index('key')['project_id'].to_dict()
+    keys=list(main['key'])
+    print("x")
+    for key in keys:
+        print("y")
+        a=read_gsheet(key, "Data Quality - General")
+        content=pd.DataFrame(a.get_all_records())
+        filtered=filtered[filtered['Status'].isin(["Pending", "Clarification Needed"])]
+        filter_add=filtered['follow_up_response']!=""
+        print("check")
+        print(key)
+        filter_add2=filtered['field_response2']=="" 
+        print("dfdf")
+        filtered=filtered[(filtered['field_response']=="") | ((filter_add) & (filter_add2)) ]
+        for index, row in filtered.iterrows():
+            text=(str(dict(row)))
+            chat_id=row['chat_id']
+            text =  "<a href='https://www.laterite.com/'>Data Quality Bot</a>" \
+            + "\n" + f"<b>Enumerator Name: </b>"+ row['Enumerator'] + \
+                "\n" +   f"<b>HHID: </b>" + str(row['HHID'])  + \
+                "\n" +   f"<b>Variable: </b>" + str(row['Variable']) \
+                +  "\n" +   f"<b>Data Quality Question :</b>" + str(row['issue_description']) \
+                +  "\n" +   f"<b>Old response :</b>" + str(row['field_response']) \
+                +  "\n" +   f"<b>Office follow up :</b>" + str(row['follow_up_response']) \
+                + "\n" + f"<b>Task :</b> Data quality" \
+            + "\n" +  f"<b>Project ID: </b> "+ dict_from_columns[key]
+            send_message(chat_id, text)
 
 
-if __name__ == "__main__":
-    ### Reading the update_id
-    main=gspread.service_account(filename='credentials.json')
-    main_key='1kq0JxL3PxB4yxZfBv_2_WOEHvy6kptP7jqB31v0XZoU'
-    sheet=main.open_by_key(main_key)
-    name_sheet='Master'
-    ms=sheet.worksheet(name_sheet)
+# if __name__ == "__main__":
+#     ### Reading the update_id
+#     main=gspread.service_account(filename='credentials.json')
+#     main_key='1kq0JxL3PxB4yxZfBv_2_WOEHvy6kptP7jqB31v0XZoU'
+#     sheet=main.open_by_key(main_key)
+#     name_sheet='Master'
+#     ms=sheet.worksheet(name_sheet)
 
-    update_id = ms.cell(1, 2).value
+#     update_id = ms.cell(1, 2).value
 
-    messages=read_msg(update_id)
-    x=pd.json_normalize(messages,)
-    # y=pd.DataFrame(messages['message'])
-    name=str(datetime.datetime.today()).replace(' ', '').replace(':', '').replace('.','')
-    x.to_excel(f'check_{name}.xlsx')
-    # y.to_excel(f'check2_{str(datetime.date.today())}.xlsx')
-    # # editing the gsheet based on the messages sent
-    for mes in messages:
-        print(mes)
-        if 'message' in mes:
-            if 'my_chat_member' in mes:
-                #my_chat_member.from.id
-                status=mes['my_chat_member']['new_chat_member']['status']
-                id=mes['my_chat_member']['from']['id']
-                new_memb=mes['my_chat_member']['new_chat_member']['user']['id']
-                #my_chat_member.new_chat_member.user.id
-                send_message_bis(585511605, status+ str(id)+ "left")
-                send_message_bis(585511605, status+ str(new_memb)+ "joined")
+#     messages=read_msg(update_id)
+#     x=pd.json_normalize(messages,)
+#     # y=pd.DataFrame(messages['message'])
+#     name=str(datetime.datetime.today()).replace(' ', '').replace(':', '').replace('.','')
+#     x.to_excel(f'check_{name}.xlsx')
+#     # y.to_excel(f'check2_{str(datetime.date.today())}.xlsx')
+#     # # editing the gsheet based on the messages sent
+#     for mes in messages:
+#         print(mes)
+#         if 'message' in mes:
+#             if 'my_chat_member' in mes:
+#                 #my_chat_member.from.id
+#                 status=mes['my_chat_member']['new_chat_member']['status']
+#                 id=mes['my_chat_member']['from']['id']
+#                 new_memb=mes['my_chat_member']['new_chat_member']['user']['id']
+#                 #my_chat_member.new_chat_member.user.id
+#                 send_message_bis(585511605, status+ str(id)+ "left")
+#                 send_message_bis(585511605, status+ str(new_memb)+ "joined")
 
-            if 'text' in mes['message'] and 'reply_to_message' not in mes['message']:
-                print("Text:"+str(mes['message']['text']))
-                print("From Id:" + str(mes['message']['from']['id']))
-                text="<a href='https://www.laterite.com/'>Data Quality Bot</a>" \
-            + "\n" + f"<b>ID: </b>"+ str(mes['message']['from']['id']) + \
-                "\n" +   f"<b>NAME: </b>" + str(mes['message']['text']) + \
-                "\n" + f"<b>USER NAME: </b>" +str(mes['message']['from']['first_name'])
-                send_message_bis(585511605, text )
-        if 'edited_message' in mes:
-            message=mes['edited_message']
-            # print(message['text'])
-            # print('edited')
-        elif 'message' in mes:
-            message=mes['message']
-            # print(message['text'])
-            # print('not edited')
-        else:
-            send_message_bis(585511605, mes['update_id'])
-            continue
-        if 'reply_to_message' in message and message['reply_to_message']['from']['first_name']== 'Data Quality Bot' and 'Number of Data Quality Items' not in message['reply_to_message']['text']:
-            # print(message)
-            pre_message_inf=message['reply_to_message']
-            if 'text' in pre_message_inf:
-                if ":" in pre_message_inf['text']:
-                    # print(pre_message_inf['text'])
-                    pre_message=str_to_dict(pre_message_inf['text'])
-                    print(pre_message)
-                    # print(pre_message_inf['text'])
-                    # print(pre_message)
-                    # print(message.keys())
-                    if 'text' in message.keys():
-                        reply_text=message['text']
-                        print(reply_text)
-                        getting_responses(pre_message, reply_text)
-                        send_message_bis(585511605, reply_text)
-                        time.sleep(2)
-                    else:
-                        #reply_text=message['voice']
-                        continue
+#             if 'text' in mes['message'] and 'reply_to_message' not in mes['message']:
+#                 print("Text:"+str(mes['message']['text']))
+#                 print("From Id:" + str(mes['message']['from']['id']))
+#                 text="<a href='https://www.laterite.com/'>Data Quality Bot</a>" \
+#             + "\n" + f"<b>ID: </b>"+ str(mes['message']['from']['id']) + \
+#                 "\n" +   f"<b>NAME: </b>" + str(mes['message']['text']) + \
+#                 "\n" + f"<b>USER NAME: </b>" +str(mes['message']['from']['first_name'])
+#                 send_message_bis(585511605, text )
+#         if 'edited_message' in mes:
+#             message=mes['edited_message']
+#             # print(message['text'])
+#             # print('edited')
+#         elif 'message' in mes:
+#             message=mes['message']
+#             # print(message['text'])
+#             # print('not edited')
+#         else:
+#             send_message_bis(585511605, mes['update_id'])
+#             continue
+#         if 'reply_to_message' in message and message['reply_to_message']['from']['first_name']== 'Data Quality Bot' and 'Number of Data Quality Items' not in message['reply_to_message']['text']:
+#             # print(message)
+#             pre_message_inf=message['reply_to_message']
+#             if 'text' in pre_message_inf:
+#                 if ":" in pre_message_inf['text']:
+#                     # print(pre_message_inf['text'])
+#                     pre_message=str_to_dict(pre_message_inf['text'])
+#                     print(pre_message)
+#                     # print(pre_message_inf['text'])
+#                     # print(pre_message)
+#                     # print(message.keys())
+#                     if 'text' in message.keys():
+#                         reply_text=message['text']
+#                         print(reply_text)
+#                         getting_responses(pre_message, reply_text)
+#                         send_message_bis(585511605, reply_text)
+#                         time.sleep(2)
+#                     else:
+#                         #reply_text=message['voice']
+#                         continue
 
 
     ### WB UPSNPJ SM
