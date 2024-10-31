861c675dc8a2174745bced11ef3233183ad98581
Bisrat Getachew <bisratgetachew0449@gmail.com>
2024-10-31 10:48:23 +0300

    ok

--- version5.py
+++ version5.py
@@ -191,7 +191,27 @@ if __name__=="__main__":
                 x= send_message(chat_id, text)
                 if x!=200:
                     send_message(585511605, f"x from {row['Enumerator']}, projec P{dict_from_columns[key]}")
-
+                
+        a=read_gsheet(key, "Data Quality - Translations")
+        content=pd.DataFrame(a.get_all_records())
+        filtered=content[content['chat_id']==chat_id]
+        ### send only pending/ clarification needed comments
+        filtered=filtered[filtered['TASK_STATUS'].isin(["Pending", "Clarification Needed"])]
+        filtered=filtered[filtered['field_response']==""]
+        for index, row in filtered.iterrows():
+            #text=(str(dict(row)))
+            chat_id=row['chat_id']
+            text2 =  "<a href='https://www.laterite.com/'>Data Quality Bot</a>" \
+            + "\n" + f"<b>Enumerator Name: </b>"+ row['enum_name'] + \
+                "\n" +   f"<b>HHID: </b>" + str(row['HHID'])  + \
+                "\n" +   f"<b>Variable: </b>" + str(row['Variable']) \
+                +  "\n" +   f"<b>Translation Item :</b>" + str(row['item_to_translate']) \
+                + "\n" + f"<b>Task :</b> Translation" \
+            + "\n" +  f"<b>Project ID: </b> "+ dict_from_columns[key]                    
+            x= send_message(chat_id, text2)
+            if x!=200:
+                send_message(585511605, f"x from {row['Enumerator']}, projec P{dict_from_columns[key]}")
+                
 
 # if __name__ == "__main__":
 #     ### Reading the update_id
